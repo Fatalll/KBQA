84a2ec973f2306dcc26455a3b7f2beecdcd38c26
Pavel Bakhvalov <pavelkrot@gmail.com>
2019-05-27 22:14:21 +0300

    fix old scripts

--- elastic_test.py
+++ elastic_test.py
@@ -1,7 +1,9 @@
 import codecs
 from elasticsearch import Elasticsearch
+from pymystem3 import Mystem
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200, 'timeout': 60, 'maxsize': 25}])
+m = Mystem()
 
 with codecs.open('dataset.csv', 'r', "utf-8") as file:
     counter = 0
@@ -10,16 +12,26 @@ with codecs.open('dataset.csv', 'r', "utf-8") as file:
         test = record[0].lower()
         result = record[1].split(';')
 
-        res = es.search(index='entity_index', size=3, body={
+        qq = []
+        an = m.analyze(test)
+        for a in an:
+            if 'analysis' in a and a['analysis']:
+                if ["x" for b in ['CONJ', 'INTJ', 'PART', 'PR', 'V', 'SPRO', 'ADV', 'APRO', 'ADVPRO'] if b in a['analysis'][0]['gr']].__len__() == 0:
+                    qq.append(a['analysis'][0]['lex'])
+            else:
+                if a['text'].rstrip():
+                    qq.append(a['text'].rstrip())
+
+        # print(qq)
+        res = es.search(index='entity_index',  body={
             "query": {
                 "bool": {
                     "must": [
                         {
                             "multi_match": {
-                                "query": test,
+                                "query": ' '.join(qq),
                                 "fields": [
-                                    "labels^5",
-                                    "labels.ngram"
+                                    "labels.shingle"
                                 ]
                             }
                         }
@@ -28,10 +40,20 @@ with codecs.open('dataset.csv', 'r', "utf-8") as file:
             }
         })
 
+        # print(res)
+        flag = False
         for hit in res['hits']['hits']:
+
             if hit['_source']['id'] in result:
                 counter += 1
-                print(test)
-                print(hit['_source']['id'], hit['_source']['labels'])
+                flag = True
+                # break
+                print("Found:", test, hit['_source']['id'], hit['_source']['labels'])
+
+        if not flag:
+            print("Not:", test, " ||| ", res['hits']['hits'])
+            print(res)
+        # else:
+        #     print("Ok", test)
 
 print(counter)
--- labels_new.py
+++ labels_new.py
@@ -10,12 +10,22 @@ try:
         sparql_query = """
         SELECT DISTINCT ?item ?label
         WHERE {{
-          VALUES ?item {{{0}}} .
+          VALUES ?item {{ {0} }}
           {{?item rdfs:label ?label}}
           UNION 
           {{?item skos:altLabel ?label}} .
-          FILTER(LANG(?label) = 'en' || LANG(?label) = 'ru')
-        }}""".format(' '.join(["wd:Q" + str(x) for x in range(offset, offset + 300, 1)]))
+          
+          ?item wikibase:statements ?pcount .
+          
+          FILTER(?pcount > 5)
+          FILTER(LANG(?label) = 'ru')
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q4167410 }}
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q1580166 }}
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q4167836 }}
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q17633526 }}
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q13406463 }}
+          FILTER NOT EXISTS {{ ?item wdt:P31 wd:Q17329259 }}
+        }} ORDER BY ?item """.format(' '.join(["wd:Q" + str(x) for x in range(offset, offset + 300, 1)]))
         results = return_sparql_query_results(sparql_query)
         print(offset)
         offset += 300
@@ -23,6 +33,7 @@ try:
         storage = dict()
 
         for result in results['results']['bindings']:
+            # print(result['item']['value'])
             if result['item']['value'].startswith('http://www.wikidata.org/entity/'):
                 entity = result['item']['value'][31:]
                 label = result['label']['value']
@@ -31,7 +42,7 @@ try:
 
                 storage[entity].add(label)
 
-        with codecs.open('labels_new.txt', 'a', "utf-8") as file:
+        with codecs.open('labels_new_v3.txt', 'a', "utf-8") as file:
             for entity in storage:
                 for label in storage[entity]:
                     file.write(entity + ":" + label + "\n")
